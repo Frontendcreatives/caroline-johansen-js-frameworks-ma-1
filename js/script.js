c7fe642289d5d127118800efd97e1cf905be075e
Caroline Johansen <caro.enga.joha@gmail.com>
2020-11-10 20:25:35 +0100

    jquery plugin image slider

--- js/script.js
+++ js/script.js
@@ -1,6 +1,50 @@
-# caroline-johansen-js-frameworks-ma-1
+var $slide = $('.slide'),
+    $slideGroup = $('.slide-group'),
+    $bullet = $('.bullet');
 
-/* images from unsplash */
+var slidesTotal = ($slide.length - 1),
+    current = 0,
+    isAutoSliding = true;
+
+$bullet.first().addClass('current');
+
+var clickSlide = function() {
+  //stop auto sliding
+  window.clearInterval(autoSlide);
+  isAutoSliding = false;
+
+  var slideIndex = $bullet.index($(this));
+
+  updateIndex(slideIndex);
+};
+
+var updateIndex = function(currentSlide) {
+  if(isAutoSliding) {
+    if(current === slidesTotal) {
+      current = 0;
+    } else {
+      current++;
+    }
+  } else {
+      current = currentSlide;
+  }
+
+  $bullet.removeClass('current');
+  $bullet.eq(current).addClass('current');
+
+  transition(current);
+};
+
+var transition = function(slidePosition) {
+    $slideGroup.animate({
+      'top': '-' + slidePosition + '00%'
+    });
+};
+
+$bullet.on( 'click', clickSlide);
+
+var autoSlide = window.setInterval(updateIndex, 2000);
+
 /* 
 
 https://codepen.io/jurbank/pen/AckDb/
